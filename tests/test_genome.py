a018adb5884c626139d760522a023a75344680f4
Jason Boutte <boutte.jason@gmail.com>
2016-02-14 19:24:20 -0800

    Fixes bad genome test

--- tests/test_genome.py
+++ tests/test_genome.py
@@ -3,5 +3,5 @@ from pyneat.genotype import Genome
 def test_genome():
     genome = Genome.minimal_fully_connected((3, 2))
 
-    assert genome.neurons == (3, 2)
+    assert genome.neurons == (3, 0, 2)
     assert len(genome.genes) == 6

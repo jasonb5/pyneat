f3086a5d7c266ca44fc1c8beca425838848433d4
Jason Boutte <boutte.jason@gmail.com>
2016-03-07 00:40:45 -0800

    Adds setup dependencies

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from distutils.core import setup
 setup(name='pyneat',
         version='1.0',
         packages=['pyneat'],
+        install_requires=[
+            'numpy',
+            'jsonpickle',
+        ],
         )

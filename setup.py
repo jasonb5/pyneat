8a534862cc67c5210fb1d58b5373548d452b1a5c
Jason Boutte <boutte.jason@gmail.com>
2016-04-03 14:19:25 -0700

    Updates setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,5 @@ from distutils.core import setup
 
 setup(name='pyneat',
         version='1.0',
-        packages=['pyneat'],
-        install_requires=[
-            'numpy',
-        ],
-        )
+        packages=['pyneat/',],
+        install_requires=['numpy', 'django'])
